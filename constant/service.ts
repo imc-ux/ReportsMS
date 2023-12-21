82fa8e302a2c448355c2c77c4dbd13a8805dedce
liujinhai8410 <272304680@qq.com>
2023-12-21 09:32:59 +0800

    提交信息

--- constant/service.ts
+++ constant/service.ts
@@ -4,4 +4,6 @@ export enum service {
   GetTemplateList = "getTemplateList", //模板管理检索
   DeleteTemplate = "deleteTemplate", //模板管理删除
   UpdateTemplate = "updateTemplate", //模板管理修改
+  GetUserTemplateList = "getUserTemplateList", //消息管理检索
+  DeleteUserTemplate = 'deleteUserTemplate',//消息管理删除
 }
