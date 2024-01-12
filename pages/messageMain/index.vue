fd3e55d4b4bdc4aba4c89acb5ed3aec606cd726c
liujinhai8410 <272304680@qq.com>
2024-01-12 16:48:00 +0800

    add waiting

--- pages/messageMain/index.vue
+++ pages/messageMain/index.vue
@@ -8,6 +8,7 @@ import { ShowAlert } from '~/components/alert';
 import { CommonAlert } from '~/constant/alert/base';
 import MarkDownTable from '~/components/MarkDownTable.vue';
 import { format } from "date-fns";
+import { setWaiting, removeWaiting } from '@/utils/loadingUtil';
 
 const router = useRouter();
 const route = useRoute();
@@ -25,6 +26,7 @@ const iPage = useState<number>('iPage', () => 1);
 const totalCount = useState<number>('totalCount', () => 0);
 const templateData = useState<any>('templateData', () => { });
 const showIndex = useState<any>('showIndex', () => true);
+const count = useState<number>('count', () => 0);
 
 const userList = async () => {
     try {
@@ -36,6 +38,7 @@ const userList = async () => {
         const res: any = await getUserList(info);
         let result = JSON.parse(res.data.value);
         if (!result.error) {
+            overCount();
             userNames.value = result.data;
         }
     } catch (error) {
@@ -49,6 +52,7 @@ const templateList = async () => {
         const res: any = await getTemplateList(info);
         let result = JSON.parse(res.data.value);
         if (!result.error) {
+            overCount();
             modelTypes.value = result.data;
         }
     } catch (error) {
@@ -73,6 +77,7 @@ const searchInfo = async () => {
         const res: any = await getUserTemplateList(info);
         let result = JSON.parse(res.data.value);
         if (!result.error) {
+            overCount();
             tableData.value = result.data;
             if (result.data && result.data.length > 0) {
                 totalCount.value = (Number)(result.data[0].totalCount);
@@ -89,12 +94,12 @@ const deleteTemplate = async () => {
     try {
         const info: TemplateHistory = {};
         info.nid = (Number)(selectDeleteNid.value);
+        setWaiting();
         const res: any = await deleteUserTemplate(info);
         let result = JSON.parse(res.data.value);
         if (!result.error) {
-            ShowAlert(CommonAlert.DELETE_DATA_SUCCESS, 0);
-            showIndex.value = true;
-            searchInfo();
+            removeWaiting();
+            ShowAlert(CommonAlert.DELETE_DATA_SUCCESS, 0, deletePageHandler);
         }
     } catch (error) {
 
@@ -103,15 +108,35 @@ const deleteTemplate = async () => {
 
 onMounted(() => {
     if (route.query?.type === 'search') {
-        onSearchHandlerClick();
+        count.value = 3;
+        iPage.value = 1;
+        searchInfo();;
+    } else {
+        count.value = 2;
     }
+    console.log(count.value);
     userList();
     templateList();
+    setWaiting();
 });
 
+function deletePageHandler() {
+    showIndex.value = true;
+    searchInfo();
+}
+
+function overCount() {
+    count.value--;
+    if (count.value === 0) {
+        removeWaiting();
+    }
+}
+
 function onSearchHandlerClick() {
+    count.value = 1;
     iPage.value = 1;
     searchInfo();
+    setWaiting();
 }
 
 function onResetHandlerClick() {
@@ -136,7 +161,7 @@ function onReturnClickHandler() {
 
 function onTabelDeleteHandler(index: number, row: any) {
     selectDeleteNid.value = row.nid;
-    deleteTemplate();
+    ShowAlert(CommonAlert.WHETHER_DELETE_MESSAGE, 2, () => deleteTemplate());
 }
 
 function handleCurrentChange(val: number) {
@@ -146,7 +171,7 @@ function handleCurrentChange(val: number) {
 
 function onDeleteTemplateMessageHandler() {
     selectDeleteNid.value = templateData.value.nid
-    deleteTemplate();
+    ShowAlert(CommonAlert.WHETHER_DELETE_MESSAGE, 2, () => deleteTemplate());
 }
 
 </script>
@@ -197,16 +222,16 @@ function onDeleteTemplateMessageHandler() {
                     </div>
                 </div>
             </div>
-            <div style="display: flex;margin-top: 5px;width: 100%">
+            <div style="display: flex;width: 100%">
                 <div style="justify-content: flex-start; display: flex; flex-grow: 1">
-                    <el-button type="primary" @click="onNewMessageHandler">新增消息</el-button>
+                    <Button class='btn-icon' @click="onNewMessageHandler">新增消息</Button>
                 </div>
                 <div style="justify-content: flex-end;display: flex;">
-                    <el-button type="primary" @click="onSearchHandlerClick">Search</el-button>
-                    <el-button type="primary" style="margin-left:5px;" @click="onResetHandlerClick">Reset</el-button>
+                    <Button class='btn-icon' @click="onSearchHandlerClick">Search</Button>
+                    <Button class='btn-icon' style="margin-left:5px;" @click="onResetHandlerClick">Reset</Button>
                 </div>
             </div>
-            <div style="margin-top: 5px">
+            <div>
                 <el-table :data="tableData" border style="width: 100%" max-height="800px">
                     <el-table-column header-align="center" align="center" prop="templateName" label="模板类型" />
                     <el-table-column header-align="center" align="center" prop="userName" label="发送人" />
